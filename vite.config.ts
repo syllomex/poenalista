639ec324696808b03d5c4041f449e264b9a90113
Leonardo Santos <leonardojdossantos@gmail.com>
2023-01-21 17:40:59 -0300

    :wrench: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      devOptions: { enabled: true },
+      registerType: 'autoUpdate',
+      workbox: {
+        skipWaiting: true,
+      },
+      manifest: {
+        name: 'Põe na Lista',
+        short_name: 'PoeNaLista',
+        description:
+          'Crie suas listas para qualquer coisa e compartilhe com quem quiser!',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+      },
+    }),
+  ],
 })
