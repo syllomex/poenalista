45fd569fc45006a5d9318f0be6a9decff02d9a73
Leonardo Santos <leonardojdossantos@gmail.com>
2023-01-21 10:25:58 -0300

    :wrench: add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './globals.css'
+
 import './services/firebase'
 
 import App from './App'
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Cedarville Cursive', 'cursive'],
+    },
+  },
+  plugins: [],
+}
